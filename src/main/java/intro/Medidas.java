4492d4f290846b733a1852ca3c3c52933d825f11
unknown <eliscrete@gmail.com>
2021-06-16 22:55:02 -0300

    Initial commit

---
+++ src/main/java/intro/Medidas.java
@@ -0,0 +1,47 @@
+
+//Pacote
+
+package intro;
+
+// Onde deverá ser referenciado as bibliotecas.
+
+
+//Classes
+public class Medidas {
+    //Atributos da classe Medidas
+
+
+
+    //Métodos e funções-  os verbos
+
+public static void main(String[] args){
+
+    System.out.println("Calculo de aréas!");
+    //Calculo de area- como calcular a area de acordo com algumas medidas.
+
+    int largura;
+    int comprimento;
+    int resultado;
+
+    largura = 4;
+    comprimento = 3;
+
+    resultado = largura * comprimento;
+    System.out.println("Para a largura de " + largura + "m e o comprimento de "+ comprimento + "m a área é de " + resultado + "m²");
+
+//  Calculo de aréa reduzido
+
+}
+
+public void calcularAreaModoCompacto(){
+
+    int largura = 4;
+    int comprimento = 3;
+
+    System.out.println("Para a largura de " + largura + "m e o comprimento de "+ comprimento + "m a área é de " + largura * comprimento + "m²");
+
+
+
+}
+
+}
